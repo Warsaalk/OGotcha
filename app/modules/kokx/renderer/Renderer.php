cd736b693fe46f67c93b4c47aec23679b6390574
Klaas Van Parys <warsaalk@gmail.com>
2014-04-15 23:15:06 +0200

    Added license comments to new files and changed the existing license comments

--- app/modules/kokx/renderer/Renderer.php
+++ app/modules/kokx/renderer/Renderer.php
@@ -1,25 +1,21 @@
 <?php
-/**
- * This file is part of Kokx's CR Converter.
+/**   OGotcha, a combat report converter for Ogame
+ *    Copyright (C) 2014  Klaas Van Parys
  *
- * Kokx's CR Converter is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Kokx's CR Converter is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Kokx's CR Converter.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @category   KokxConverter
- * @license    http://www.gnu.org/licenses/gpl.txt
- * @copyright  Copyright (c) 2009 Kokx
- * @package    Default
- * @subpackage Renderer
+ *   This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *   
+ *    This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details. 
+ *   
+ *    You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ *   This program is based on the Kokx's CR Converter © 2009 kokx: https://github.com/kokx/kokx-converter
  */
 
 /**
--- app/modules/kokx/renderer/vii/tpl/lossesmoon.tpl.php
+++ app/modules/kokx/renderer/vii/tpl/lossesmoon.tpl.php
@@ -1,4 +1,23 @@
 <?php
+/**   OGotcha, a combat report converter for Ogame
+ *    Copyright (C) 2014  Klaas Van Parys
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   This program is based on the Kokx's CR Converter © 2009 kokx: https://github.com/kokx/kokx-converter
+ */
+
 print $self->translate("The attacker lost a total of %s units.", $self->colorNumber($self->_report->getLossesAttacker())) . "\n";
 print $self->translate("The defender lost a total of %s units.", $self->colorNumber($self->_report->getLossesDefender())) . "\n";
 print $self->translate("At these space coordinates now float ") . $self->colorNumber($self->_report->getDebrisMetal()) . " " . $self->translate("metal") . " " . $self->translate("and") . " " . $self->colorNumber($self->_report->getDebrisCrystal()) . " " . $self->translate("crystal.") . "\n";
