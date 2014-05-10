c59e94515a6efc3e6b434413798931f087745941
Klaas Van Parys <warsaalk@gmail.com>
2014-05-10 17:21:05 +0200

    Changed default language to English

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $main = new Main(); //Create Main
 //$main->addInfo( new Info( "This website is still under construction and can be used for testing purposes only! Errors can occur when doing certain actions.", Info::INFORMATION ) );
 //$main->addInfo( new Info( "Copyrights are not final yet. This software is under GNU GPL v3 license, the source will be available when the software is finished!", Info::INFORMATION ) );
 
-$main->initDictionaries( array('nl','en') );
+$main->initDictionaries( array('nl','en'), 'en' );
 $main->initPages( array('home','info') );
 
 $main->setValidator( new Validator() ); //Connect form validator
